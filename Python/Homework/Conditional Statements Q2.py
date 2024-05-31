0a1fa3069aa23ff5e304dd0c99d6c8164164e944
Kush Milkesh Mistry <137867286+Kush27110405@users.noreply.github.com>
2024-05-31 19:05:06 +0530

    Create Conditional Statements Q2.py

---
+++ Python/Homework/Conditional Statements Q2.py
@@ -0,0 +1,17 @@
+# to assign grade based on score (0-100) using nested if-else
+
+score = int(input("Enter the score: "))
+
+if score >= 90 :
+  print("A")
+else :
+  if score >= 80 :
+    print("B")
+  else:
+    if score >= 70 :
+      print("C")
+    else :
+      if score >= 60 :
+        print("D")
+      else :
+        print("F")
