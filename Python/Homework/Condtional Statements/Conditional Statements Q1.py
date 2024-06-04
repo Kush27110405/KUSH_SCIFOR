eb050b50d398cb6bfc807a24bcaf5fc2ab15baa5
Kush Milkesh Mistry <137867286+Kush27110405@users.noreply.github.com>
2024-06-04 21:17:44 +0530

    Delete Python/Homework/Condtional Statements/Conditional Statements Q1.py

--- Python/Homework/Condtional Statements/Conditional Statements Q1.py
+++
@@ -1,8 +0,0 @@
-# Question 1 :- to check if a number is odd using if-else
-
-num = int(input("Enter a number: "))
-
-if num % 2 != 0 :
-  print("The number is odd")
-else:
-  print("The number is even")
