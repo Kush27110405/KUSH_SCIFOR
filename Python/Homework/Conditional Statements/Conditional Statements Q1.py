c47f0c64eda6607392015438a543850ff1dd1e81
Kush Milkesh Mistry <137867286+Kush27110405@users.noreply.github.com>
2024-06-04 21:18:20 +0530

    Create Conditional Statements Q1.py

---
+++ Python/Homework/Conditional Statements/Conditional Statements Q1.py
@@ -0,0 +1,8 @@
+# Question 1 :- to check if a number is odd using if-else
+
+num = int(input("Enter a number: "))
+
+if num % 2 != 0 :
+  print("The number is odd")
+else:
+  print("The number is even")
