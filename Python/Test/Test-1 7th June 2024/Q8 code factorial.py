59fdb189e5d139823b90b4386a50ad7b94e72ec5
Kush Milkesh Mistry <137867286+Kush27110405@users.noreply.github.com>
2024-06-07 20:22:33 +0530

    Create Q8 code factorial.py

---
+++ Python/Test/Test-1 7th June 2024/Q8 code factorial.py
@@ -0,0 +1,12 @@
+"""
+Q8:- code for factorial (using for loop)
+"""
+
+def factorial(n):
+  result = 1
+  for i in range(1, n+1):
+    result *= i
+  return result
+
+n = int(input("Enter a number: "))
+print(f"The factorial of {n} is {factorial(n)}")
