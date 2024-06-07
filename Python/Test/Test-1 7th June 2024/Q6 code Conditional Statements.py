94a457601fca8373f0941192f71f6904b8ceae21
Kush Milkesh Mistry <137867286+Kush27110405@users.noreply.github.com>
2024-06-07 20:19:46 +0530

    Create Q6 code Conditional Statements.py

---
+++ Python/Test/Test-1 7th June 2024/Q6 code Conditional Statements.py
@@ -0,0 +1,13 @@
+"""
+Q6:- code for conditional statements
+"""
+
+num = float(input("Enter a decimal number: "))
+
+if num > 0:
+  print("Positive")
+else:
+  if num < 0:
+    print("Negative")
+  else:
+    print("Zero")
